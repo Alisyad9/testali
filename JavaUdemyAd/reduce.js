bd62c86737f2540e6bdd74f9213500208e9a5fcd
Alisiyad9 <alisiyad9@gmail.com>
2021-08-27 22:40:21 +0100

    reduce

--- JavaUdemyAd/reduce.js
+++ JavaUdemyAd/reduce.js
@@ -1,14 +1,13 @@
-let vals=[1,3,4,4,1,1];
-function add(acc, val) {
-       if (val >acc) {
-         acc=val;
-         
-        }
+// let vals=[1,3,4,4,1,1];
+// function add(acc, val) {
+//        if (val >acc) {
+//          acc=val;
 
-  return acc;
-       
-  
-}
-let answer = vals.reduce(add);
+//         }
+
+//   return acc;
+
+// }
+// let answer = vals.reduce(add);
 
 console.log(answer);
